cb508dc9bf177330417f9b162b8c26c739a1b16c
liguangxin <965322027@qq.com>
2021-10-31 09:44:29 +0800

    feat(初始化仓库):

---
+++ mbesos-admin/src/main/java/com/mbesos/blog/MBesosApplication.java
@@ -0,0 +1,17 @@
+package com.mbesos.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liGuAngXin
+ * @date 2021-10-30 14:44
+ * @classname MBesosApplication
+ * @description
+ */
+@SpringBootApplication
+public class MBesosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MBesosApplication.class, args);
+    }
+}
